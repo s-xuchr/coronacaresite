f6c2b4715a7d043aeb518d25bb7185b8f3182585
Christianna Xu <christianna.xu@berkeley.edu>
2020-06-14 09:33:47 -0700

    copied files in

---
+++ build/precache-manifest.a747749a6bea1334d9970890e285a75a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22a951289668f482624a863d740979f",
+    "url": "/coronacare/index.html"
+  },
+  {
+    "revision": "e81e7cbcae9ddaa378f3",
+    "url": "/coronacare/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d55e403cfa4ca33bc97a",
+    "url": "/coronacare/static/css/main.18c4b241.chunk.css"
+  },
+  {
+    "revision": "e81e7cbcae9ddaa378f3",
+    "url": "/coronacare/static/js/2.12f292de.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/coronacare/static/js/2.12f292de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d55e403cfa4ca33bc97a",
+    "url": "/coronacare/static/js/main.23eea3f9.chunk.js"
+  },
+  {
+    "revision": "def2a14c9b9061567133",
+    "url": "/coronacare/static/js/runtime-main.f2f9b222.js"
+  }
+]);
