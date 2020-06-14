256b07b185aec0de13911d887cc0909aaa95f6e6
Christianna Xu <christianna.xu@berkeley.edu>
2020-06-14 13:12:07 -0700

    minor details added to contact us page

--- src/NoMatch.js
+++ src/NoMatch.js
@@ -8,5 +8,6 @@ import styled from 'styled-components';
 export const NoMatch = () => (
   <div className="padding">
     <h1 style={{fontFamily: 'Verdana'}}>Contact Us</h1>
+    <p>Check out our Facebook page at <a href="https://www.facebook.com/coronacareai/">https://www.facebook.com/coronacareai/</a>!</p>
   </div>
 )
