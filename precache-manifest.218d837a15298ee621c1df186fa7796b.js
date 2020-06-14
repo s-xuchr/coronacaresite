2157310602e17ba0facdae8d07058d402b2c9874
Christianna Xu <christianna.xu@berkeley.edu>
2020-06-14 09:40:14 -0700

    Updates

---
+++ precache-manifest.218d837a15298ee621c1df186fa7796b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d02c7f72bb7619583906b1e8709542d",
+    "url": "/coronacaresite/index.html"
+  },
+  {
+    "revision": "daeb22a13e3d46849331",
+    "url": "/coronacaresite/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "225f478109d0a7613fad",
+    "url": "/coronacaresite/static/css/main.18c4b241.chunk.css"
+  },
+  {
+    "revision": "daeb22a13e3d46849331",
+    "url": "/coronacaresite/static/js/2.bc8c6184.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/coronacaresite/static/js/2.bc8c6184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225f478109d0a7613fad",
+    "url": "/coronacaresite/static/js/main.48ee064b.chunk.js"
+  },
+  {
+    "revision": "0795f2196375f9006cb0",
+    "url": "/coronacaresite/static/js/runtime-main.d3cd82de.js"
+  }
+]);
